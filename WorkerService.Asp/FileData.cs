9f98e19277b980f329c9094867a7079a4933183a
Carlos Gimenez <carlosgibe@gmail.com>
2024-08-28 18:21:01 +0200

    created asp project and structure

---
+++ WorkerService.Asp/FileData.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkerService.Asp
+{
+    public class FileData : IFileData
+    {
+        public async Task Create(string path)
+        {
+            using (var sw = new StreamWriter(path))
+            {
+                await Task.Delay(1000);
+                await sw.WriteAsync("tux");
+            }
+        }
+    }
+}
---
+++ WorkerService.Asp/IFileData.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkerService.Asp
+{
+    public interface IFileData
+    {
+        public Task Create(string Path);
+    }
+}
